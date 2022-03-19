ee591d471611f7f37885f996f16562af2a7f5779
E <pucadesignstudio@gmail.com>
2022-03-18 18:43:27 -0600

    Bootup Template

---
+++ README.md
@@ -0,0 +1,13 @@
+Make sure Python 3.5 or higher is installed on your system.
+Open this directory in a command prompt, then:
+
+1. Install the software:
+   ```
+   pip install -r requirements.txt
+   ```
+
+2. Run the development server:
+   ```
+   python manage.py runserver
+   ```
+
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-9w@vftf-sw0bgey@)e3(m+f5bm1k#754+0l%7^%lp861odr$l'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
